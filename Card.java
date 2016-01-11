bf87c80a83bd880332003a9ea6d1d5c6673d24d0
shamaul-d <sdilmohamed@stuy.edu>
2016-01-11 10:13:57 -0500

    created Card.java

---
+++ Card.java
@@ -0,0 +1,12 @@
+/*
+  Niels Graham, Shamaul Dilmohamed 
+  APCS1 pd10
+  Final Project -- Texas Hold'em Poker
+  2016-1-11
+*/
+
+
+public class Card {
+    private String value;
+    private String final 
+}
