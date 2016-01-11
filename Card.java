87dd6c8c0039a2976075bf92d08e11306ca4a187
shamaul-d <sdilmohamed@stuy.edu>
2016-01-11 10:25:08 -0500

    wrote instance vars for Card.java

--- Card.java
+++ Card.java
@@ -8,5 +8,11 @@
 
 public class Card {
     private String value;
-    private String final 
+
+    private final String[] vals = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    // the list of values that a card can hold
+
+    private final String[] suits = {"diamond", "clubs", "hearts", "spades"};
+    // the list of suits a card can have
+ 
 }
