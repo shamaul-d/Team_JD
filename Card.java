bac9609ef4b4a573b040e6c8fc1b16a7b9cc69ba
shamaul-d <sdilmohamed@stuy.edu>
2016-01-12 10:24:19 -0500

    Card.java done

--- Card.java
+++ Card.java
@@ -8,10 +8,10 @@
 
 public class Card {
     private String value;
-    private String cardLine = "\t|               |\n";
+    private String cardLine = "\t|               |\n"; //body of each playing card
 
     /*
-    private final String[] vals = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    private final String[] vals = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
     // the list of values that a card can hold
 
     private final String[] suits = {"diamond", "club", "heart", "spade"};
@@ -19,39 +19,39 @@ public class Card {
     */ //all constants will go in Deck
 
     public Card(String val, String suit) {
-	value = "\t_________________\n";
-	value += "\t|" + suit + cardLine.substring(suit.length()+2);
+	value = "\t_________________\n"; //top of card
+	value += "\t|" + suit + cardLine.substring(suit.length()+2); //suit name in upper left hand corner
 	for (int i = 0; i < 4; i++) {
-	    value += cardLine;
+	    value += cardLine; //body of card
 	}
-	value += cardLine.substring(0,cardLine.length()-val.length()-9) + val + cardLine.substring(cardLine.length()-val.length()-8);
+	value += cardLine.substring(0,cardLine.length()-val.length()-9) + val + cardLine.substring(cardLine.length()-val.length()-8); // value in middle of card
 	for (int i = 0; i < 4; i++) {
-	    value += cardLine;
+	    value += cardLine; //body of card
 	}
-	value += cardLine.substring(0,cardLine.length()-suit.length()-2) + suit + "|\n";
+	value += cardLine.substring(0,cardLine.length()-suit.length()-2) + suit + "|\n"; //suit at bottom right hand corner
 	value += "\t";
-	for (int x = 0; x < 17; x++) {
-	    value += Character.toString((char)175); // aesthetics 
+	for (int x = 0; x < 17; x++) { //just so happens that each line is 17 characters long
+	    value += Character.toString((char)175); // aesthetics; bottom of card 
 	}
 	value += "\n";
 	    }
 
-    public String getCard() {
+    public String toString() {
 	return value;
     }
 
     public static void main(String[] args) {
 	Card sham = new Card("9","heart");
 	System.out.println("Shamaul:");	
-	System.out.println(sham.getCard());
+	System.out.println(sham);
 	Card niels = new Card ("A","spade");
 	System.out.println("Niels:");	
-	System.out.println(niels.getCard());
+	System.out.println(niels);
 	Card kate = new Card ("3","diamond");
 	System.out.println("Kate:");	
-	System.out.println(kate.getCard());
+	System.out.println(kate);
 	Card brown = new Card ("K","club");
 	System.out.println("Mr. Brown:");	
-	System.out.println(brown.getCard());
+	System.out.println(brown);
     }
 }
