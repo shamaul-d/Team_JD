340ab719060a806d096155348a93cb027ff7bba0
cowson <ngraham@gmail.com>
2016-01-24 16:33:13 -0500

    ?

--- Poker.java
+++ Poker.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import cs1.Keyboard;
 
 public class Poker {
+    static Player[] pl;
+    static String[] names;
     
     public static void start() {
 	String response = Keyboard.readString();
@@ -32,9 +34,59 @@ public class Poker {
 	    start();
 	}
     }
-    
+
+    public static void setup() {
+	System.out.print("How many players want to play? (1-12): ");
+	String numS = Keyboard.readString();
+	int num = 0;
+	try {
+	    num = Integer.parseInt(numS);
+	    if (num < 0) {
+		System.out.println("We're pretty sure the number of people playing has to be positive...");
+		setup();
+		return;
+	    }
+	    else {
+		names = new String[num];
+		pl = new Player[num];
+		for (int x = 1; x < num + 1; x++) {
+		    System.out.println("Name of player "+ x + " :");
+		    String name = Keyboard.readString();
+		    names[x-1] = name;
+		}
+	    }
+	}
+	catch (Exception e) {
+	    System.out.println("A number would be nice, please.");
+	    setup();
+	    return;
+	}
+    }
+	
+    public static void choice() {
+	String ch = Keyboard.readString();
+	if (ch.equals("d")) {
+	    for (int x = 0; x < names.length; x++) {
+		pl[x] = new Player();
+	    }
+	}
+	else if (ch.equals("c")) {
+	    System.out.println("How many chips would you like?");
+	    for (int x = 0; x < names.length; x++) {
+		pl[x] = new Player();
+	    }
+	}
+	else {
+	    System.out.println("Wouldn't you like to start our beautiful game as quickly as possible? It's pretty good, if we say so ourselves.");
+	    choice();
+	}
+    }
+  
     public static void main(String[] args) {
 	System.out.print("Welcome to Texas Hold'em Poker! Would you like a list of rules before we begin, or are you ready to go? (type y or n): ");
 	start();
+	setup();
+	System.out.println("Would you like to start with the default number of chips, or a custom amount? (default is 100) (type d or c): ");
+	//choice();
     }
 }
