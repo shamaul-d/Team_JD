eb1179fe4ee5fe3cef778ed65b791c8332f4567f
shamaul-d <sdilmohamed@stuy.edu>
2016-01-24 07:13:01 -0500

    started work on Poker (driver) class

---
+++ Poker.java
@@ -0,0 +1,11 @@
+// driver for poker game
+
+import cs1.Keyboard;
+
+public class Poker {
+
+    public static void main(String[] args) {
+	System.out.println("Welcome to Texas Hold'em Poker! Would you like a list of rules before we begin, or are you ready to go? (type y or n)");
+       
+    }
+}
