e0d294994a16f9ce66bb814b39ca1998cc864a67
shamaul-d <sdilmohamed@stuy.edu>
2016-01-24 11:06:19 -0500

    Poker.java goes all the way up to initialzing players and chips.

--- Poker.java
+++ Poker.java
@@ -36,13 +36,24 @@ public class Poker {
     }
 
     public static void setup() {
-	System.out.print("How many players want to play? (1-12): ");
+	System.out.print("How many players want to play? (2-12): ");
 	String numS = Keyboard.readString();
 	int num = 0;
 	try {
 	    num = Integer.parseInt(numS);
-	    if (num < 0) {
+	    if (num <= 0) {
 		System.out.println("We're pretty sure the number of people playing has to be positive...");
+		setup();
+		return;
+	    }
+	    if (num == 1) {
+		System.out.println("You can't play poker with only one player. That's kinda sad.");
+		setup();
+		return;
+	    }
+	    
+	    else if (num > 12) {
+		System.out.println("That's too many players. We refuse.");
 		setup();
 		return;
 	    }
@@ -70,23 +81,45 @@ public class Poker {
 		pl[x] = new Player();
 	    }
 	}
-	else if (ch.equals("c")) {
-	    System.out.println("How many chips would you like?");
-	    for (int x = 0; x < names.length; x++) {
-		pl[x] = new Player();
-	    }
+	else if (ch.equals("c")) { 
+	    startingChips();
 	}
 	else {
-	    System.out.println("Wouldn't you like to start our beautiful game as quickly as possible? It's pretty good, if we say so ourselves.");
+	    System.out.print("Wouldn't you like to start our beautiful game as quickly as possible? It's pretty good, if we say so ourselves. (type d or c) : ");
 	    choice();
 	}
     }
-  
+    
+    public static void startingChips() {
+	String n;
+	int c;
+	System.out.println("How many chips would you like?");
+	n = Keyboard.readString();
+	try {
+	    c = Integer.parseInt(n);
+	    if (c <= 0) {
+		System.out.println("We're pretty sure the number of chips has to be positive...");
+		startingChips();
+		return;
+	    }
+	    else {
+		for (int x = 0; x < names.length; x++) {
+		    pl[x] = new Player(c);
+		}
+	    }
+	}
+	catch (Exception e) {
+	    System.out.println("Chip values are to be taken seriously.");
+	    startingChips();
+	    return;
+	}
+    }
+   
     public static void main(String[] args) {
 	System.out.print("Welcome to Texas Hold'em Poker! Would you like a list of rules before we begin, or are you ready to go? (type y or n): ");
 	start();
 	setup();
 	System.out.println("Would you like to start with the default number of chips, or a custom amount? (default is 100) (type d or c): ");
-	//choice();
+	choice();
     }
 }
